7d2daadb10c22156247d1b33db04d685a481fcd8
Ndanema98 <davidnda26@yahoo.co.uk>
2022-08-17 14:15:25 +0000

    Feat: Add superuser and build admin site

--- reviews/models.py
+++ reviews/models.py
@@ -7,7 +7,7 @@ from cloudinary.models import CloudinaryField
 STATUS = ((0, "Draft"), (1, "Published"))
 
 
-class Reviews(models.Model):
+class Post(models.Model):
     title = models.CharField(max_length=200, unique=True)
     slug = models.SlugField(max_length=200, unique=True)
     author = models.ForeignKey(User, on_delete=models.CASCADE, related_name="review_post")
