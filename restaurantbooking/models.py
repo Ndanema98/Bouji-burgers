5bc6a0b154d56e255a86f1f2f860be91d7f656c1
Ndanema98 <davidnda26@yahoo.co.uk>
2022-08-22 16:06:55 +0000

    Feat: Add models for booking system

--- restaurantbooking/models.py
+++ restaurantbooking/models.py
@@ -1,3 +1,16 @@
 from django.db import models
 
 # Create your models here.
+
+class Customer(models.Model):
+    email = models.EmailField()
+   
+class Table(models.Model):
+    seats = models.IntegerField()
+    min_people = models.IntegerField()
+    max_people = models.IntegerField()
+
+class Reservation(models.Model):
+    table = models.ForeignKey('Table', on_delete=models.CASCADE)
+    party = models.ForeignKey('Customer', on_delete=models.CASCADE)
+    spot = models.DateField()
--- reviews/admin.py
+++ reviews/admin.py
@@ -6,8 +6,15 @@ from .models import Post
 @admin.register(Post)
 class PostAdmin(SummernoteModelAdmin):
 
+    list_display = ('title','slug','status','created_on')
+    search_fields = ['title','content']
+    prepopulated_fields = {'slug': ('title',)}
+    list_filter = ('status','created_on')
     summernote_fields = ('content',)
+    actions = ['approve_post']
 
+    def approve_post(self, request, queryset):
+        queryset.update(approved=True)
 
 
 
